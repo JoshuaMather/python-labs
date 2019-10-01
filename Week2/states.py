b9e598d5ffcb8b870c48675d90c9bd87d5a94674
p54507jm <joshua.mather@student.manchester.ac.uk>
2019-10-01 15:55:24 +0100

    up to arrays

--- Week2/states.py
+++ Week2/states.py
@@ -1,11 +1,20 @@
 def printState():
-    print ("Red Light is " + redLight)
-    print ("Yellow Light is " + yellowLight)
-    print ("Green Light is " + greenLight)
-
-redLight = "on"
-yellowLight = "off"
-greenLight = "off"
+    print ("Red Light is " + str(redLight))
+    print ("Yellow Light is " + str(yellowLight))
+    print ("Green Light is " + str(greenLight))
+    print(int(3)+5)
+redLight = True
+yellowLight = False
+greenLight = False
 
 printState()
 print(type(redLight))
+
+x = 10
+y = 20.0
+z = 1j
+
+print(type(x), type(y), type(z))
+
+day = "Beautiful"
+print(day[1])
