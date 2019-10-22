ff8b6ceb3191ffc297d6ead2d705c7adad49c7c9
p54507jm <joshua.mather@student.manchester.ac.uk>
2019-10-22 16:00:51 +0100

    finished tasks

--- Week5/hangman.py
+++ Week5/hangman.py
@@ -1,15 +1,30 @@
 import random
 
-with open("englishWords.txt", "r") as file:
-    wordlist = file.readline
-wordlist = [line.rstrip('\n') for line in open("englishWords.txt")]
+#with open("englishWords.txt", "r") as file:
+    #wordlist = file.readline
+#wordlist = [line.rstrip('\n') for line in open("englishWords.txt")]
+print("1. Easy " + "\n2. Medium" + "\n3. Hard")
+difficulty = input("Enter a difficulty level 1, 2 or 3: ")
+
+
+wordlist= []
+file = open("englishWords.txt", "r")
+for line in file:
+        line = line.rstrip()
+        if (len(line) >= 10 and difficulty == "1"):
+            wordlist.append(line)
+        elif (len(line) > 5 and difficulty == "2"):
+            wordlist.append(line)
+        elif (len(line) <= 5 and difficulty == "3"):
+            wordlist.append(line)
 
 win = False
 word = random.choice(wordlist)
 count = 0
 guesses = ""
 answer = ""
-for a in range(0, len(word)):
+charnumber = len(word)
+for a in range(0, charnumber):
     answer += "_"
 
 while count < 10 and win == False:
